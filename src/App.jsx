464af7b8b4fd08ed994a9b58ef2d0995c92b7c87
midlajvp <mmidlaj7902@gmail.com>
2026-09-14 21:11:07 +0530

    APP BUG EDIT

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import "./app.css";
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import { ShieldAlert, LogOut } from "lucide-react";
 
